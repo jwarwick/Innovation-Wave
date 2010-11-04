ae7686f6cf289b3f7db705e90356d6ccb722d2fc
jwarwick <jwarwick@gmail.com>
2010-11-04 16:30:29 -0400

    Simple sequel database working

--- Gemfile
+++ Gemfile
@@ -1,1 +1,5 @@
 gem "sinatra"
+gem "sequel"
+gem "sinatra-sequel"
+gem "sqlite3-ruby"
+
--- iw_server.rb
+++ iw_server.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require 'sinatra/sequel'
+
+set :database, ENV['DATABASE_URL'] || 'sqlite://my.db'
+
+migration "create the projects table" do
+  database.create_table :projects do
+    primary_key :id
+    text        :name, :unique => true
+  end
+end
+
+class Projects < Sequel::Model
+end
 
 get '/' do
-  'Hello World'
+  # Projects.insert(:name => 'proj 2')
+  # Projects[1].name
+  projStr = "Project count: #{Projects.count}\n"
+  Projects.each{ |p| projStr << "\t#{p.name}"}
+  projStr
 end
+
