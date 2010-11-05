f1aeee590f30ffcf6224eab30b0f6f09b6ef63c7
jwarwick <jwarwick@gmail.com>
2010-11-05 11:39:34 -0400

    Added haml views for index and project

--- iw_server.rb
+++ iw_server.rb
@@ -1,8 +1,13 @@
 require 'sinatra'
 require 'sinatra/sequel'
 require 'json'
+require 'haml'
 
 set :database, ENV['DATABASE_URL'] || 'sqlite://my.db'
+set :haml, :format => :html5
+
+# required to keep RVM happy about where the haml views are located
+set :views, File.dirname(__FILE__) + '/views'
 
 migration "create the projects table" do
   database.create_table :projects do
@@ -17,16 +22,17 @@ end
 get '/' do
   # Projects.insert(:name => 'proj 2')
   # Projects[1].name
-  projStr = "Project count: #{Projects.count}\n"
-  Projects.each{ |p| projStr << "\t#{p.id} #{p.name}"}
-  projStr
+  # projStr = "Project count: #{Projects.count}\n"
+  # Projects.each{ |p| projStr << "\t#{p.id} #{p.name}"}
+  @projects = Projects.all
+  haml :index
 end
 
 get '/projects/:id/?' do
   @proj = Projects[params[:id]]
   halt [404, "No such project"] if @proj.nil?
   
-  @proj.name
+  haml :project
 end
 
 post '/projects/?' do
--- utility.rb
+++ utility.rb
@@ -1,5 +1,9 @@
 require 'rest_client'
 require 'json'
 
-response = RestClient.post "http://127.0.0.1:4567/projects", { 'name' => '' }.to_json, :content_type => :json, :accept => :json
-puts "#{response.code}: #{response.to_str}"
+begin
+  response = RestClient.post "http://127.0.0.1:4567/projects", { 'name' => '' }.to_json, :content_type => :json, :accept => :json
+  puts "#{response.code}: #{response.to_str}"
+rescue => e
+  puts "Exception: #{e.http_code}: #{e.response}"
+end
